042a74258b1ffdd23934de481f6da1a9447d1f51
Teguhriyadi <ilham.teguh55@gmail.com>
2021-12-12 07:37:11 +0700

    revisi template

---
+++ resources/views/layouts/partials_pelatih/js/style_js.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ url('/template') }}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ url('/template') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{ url('/template') }}/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ url('/template') }}/dist/js/demo.js"></script>
